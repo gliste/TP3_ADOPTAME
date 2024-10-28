4b0cee59c21b7ba79483858d047b960aaf6475b2
Gabriela <gabrielaliste@hotmail.com>
2024-10-27 23:08:39 -0300

    AdoptameConNavegacion

--- app/src/main/java/com/example/adoptme/MainActivity.kt
+++ app/src/main/java/com/example/adoptme/MainActivity.kt
@@ -5,80 +5,68 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontStyle
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import com.example.adoptme.navigation.AppNavigation
+import com.example.adoptme.screens.FirstScreen
 import com.example.adoptme.ui.theme.AdoptMeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        enableEdgeToEdge() //se habilita el diseño de pantalla completo
+//        setContent { //se establece el contenido de la actividad utilizando un tema personalizado
+//            AdoptMeTheme {
+//               //Scaffold: se utiliza para estructurar la interfaz de usuario
+//                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+//                    Greeting(
+//                        name = "Inicio",
+//                        modifier = Modifier.padding(innerPadding)
+//                    )
+//
+//                }
+//            }
+//        }
         setContent {
             AdoptMeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Inicio",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                    //TextMensaje(
-                    //    "Adopción de Perritos"
-                    //            FontStyle.Normal
-                    //            FontWeight.Bold
-                    //)
+                Surface (color = MaterialTheme.colorScheme.background){
+                    AppNavigation()
                 }
             }
         }
     }
 }
-
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-
-    Surface(color = Color.White , modifier = Modifier.fillMaxSize()) {
-
-            Image(
-                painterResource(R.drawable.concepto_adopcion_mascotas_lindo_perro),
-                contentDescription = "perrito",
-                modifier = Modifier.width(100.dp)
-
-            )
-            //Button() { }
-
-
-    }
-    Text( text = " $name",
-             fontSize = 15.sp,
-         color = Color.DarkGray)
-}
-
-@Composable
-
-fun TextMensaje(text: String, fontStyle: FontStyle, fontWeight: FontWeight) {
-     "Adopción de Perritos"
-     FontStyle.Normal
-     FontWeight.Bold;
-}
-
 @Preview(showBackground = true)
+//@Composable
+//    fun GreetingPreview() {
+//        AdoptMeTheme {
+//            Greeting("Inicio")
+//        }
+//    }
+
 @Composable
-fun GreetingPreview() {
+fun DefaultPreview(){
     AdoptMeTheme {
-        Greeting("Inicio" )
+         AppNavigation()
     }
 }
+
+
+
